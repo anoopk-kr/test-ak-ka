a87145a8581f8d66a4c75c59f80991a7f91bfa4d
anoopk-kr <58791511+anoopk-kr@users.noreply.github.com>
2022-05-17 01:23:30 -0400

    Update updated.sh

--- updated.sh
+++ updated.sh
@@ -5,28 +5,30 @@ SECONDS=0
 OLDIFS=$IFS
 
 
-#IFS=''   
-file='repolist.txt'
-echo "$file"
-while read i
-	do
-	CWD=$PWD	
-  cd $CWD/"$(echo $i | tr -d '\r')"
-	echo "$i"
+#IFS=''
+cat repolist |
+        while read i
+        do
+        CWD=$PWD
+        cd $CWD/"$i"
+        echo "$i"
+        OIFS="$IFS"
+        IFS=$'\n'
         git_files=$(git ls-files | grep -iEv 'yaml|yml|readme')
-        for git_file in $git_files; do		
+        for git_file in $git_files; do
         encoding=$(file --mime-encoding "$git_file")
         type=$(echo "$encoding" | awk -F ':' '{print $2}')
         if [ "$type" == " binary" ]; then
         echo -e "$git_file is a ${RED}binary${NC} file"
-        fi  
-        done    
-        cd $CWD	
-        done < $file
+        fi
+        done
+        IFS="$OIFS"
+        cd $CWD
+        done
 RED='\033[0;31m'
 NC='\033[0m'
-	echo -e "${RED}Other executable files found are${NC}"
-        find . -type f -executable -print  
+        echo -e "${RED}Other executable files found are${NC}"
+        find . -type f -executable -print
         echo "Scan Endtime is `date`"
         duration=$SECONDS
         echo "$(($duration / 60)) minutes and $(($duration % 60)) seconds elapsed."
