12c76e1a8a0a8b7bd25fe780e45fde550a51f024
anoopk-kr <anoop.kota@kroger.com>
2026-05-06 21:21:34 -0400

    my role assignment

--- asd.tf
+++ asd.tf
@@ -1,5 +1,5 @@
 resource "azurerm_role_assignment" "monitoring_reader" {
-  principal_id         = "1120-112132-32321-23121"
+  principal_id         = "hhasass--11=ssdsda1"
   role_definition_name = "Monitoring Reader"
   scope                = data.azurerm_subscription.current.id
 }
